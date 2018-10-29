37f6eada7f60dceb36c824c3e9ef15109a8a9695
Ahmadreza Zibaei <ahmadrezazibaei@hotmail.com>
2018-10-29 13:58:42 +0330

    update test files

---
+++ lexer/lexer_test.go
@@ -0,0 +1,42 @@
+package lexer
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNewLexer(t *testing.T) {
+	lexer := NewLexer()
+	err := lexer.Load(bytes.NewBufferString(`
+	### Hello World
+	a := 100;
+	b := 200;
+	c := a + b;
+	`))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("String reader: OK !")
+}
+
+func TestTokenizer(t *testing.T) {
+	lexer := NewLexer()
+	err := lexer.Load(bytes.NewBufferString(`	
+	a := 100;
+	`))
+	if err != nil {
+		t.Fatal(err)
+	}
+	variableDetector := false
+	for token := range lexer.Tokenizer() {
+		if token.Text == "a" {
+			variableDetector = true
+			break
+		}
+	}
+	if !variableDetector {
+		t.Log("Did not detect variable !")
+	} else {
+		t.Log("Variable detected !")
+	}
+}
--- main_test.go
+++ main_test.go
@@ -11,14 +11,12 @@ func TestMain(t *testing.T) {
 	f, err := os.Open("example.mo")
 	if err != nil {
 		t.Fatal(err)
-		return
 	}
 
 	lexer := lx.NewLexer()
 	err = lexer.Load(f)
 	if err != nil {
 		t.Fatal(err)
-		return
 	}
 
 	t.Log("Reading example file successfully finished!")
